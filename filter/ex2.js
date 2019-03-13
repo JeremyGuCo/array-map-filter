f94c6d44393741df5ee151b05d35304597e0cb27
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-03-13 10:23:42 +0100

    Ex2 Filter

--- filter/ex2.js
+++ filter/ex2.js
@@ -16,6 +16,10 @@ Exemple d'entrée:
  */
 
 function getMultiplesOf(numbers, n) {
+  const multNum = numbers.filter(function(num){
+    return num % n === 0
+  })
+  return multNum
 }
 
 // Ne pas modifier l'export
